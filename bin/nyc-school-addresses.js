792b78dee7cd67d9e487847bb6b715cb4ece1042
Atul Varma <varmaa@gmail.com>
2014-12-30 17:52:59 -0500

    add fixups for unrecognized addresses.

--- bin/nyc-school-addresses.js
+++ bin/nyc-school-addresses.js
@@ -2,11 +2,20 @@ var fs = require('fs');
 var csv = require('csv');
 var stableStringify = require('json-stable-stringify');
 
+var ADDRESS_FIXUPS = {
+  "1 CORPORATE COMMONS1 TELEPORT": "1 Teleport Drive, Staten Island, NY 10311",
+  "250 EAST 156TH ST4TH FL": "250 East 156th Street, Bronx, NY 10451"
+};
 var BASENAME = "data/nyc-school-addresses";
 var CSV_FILENAME = __dirname + "/../" + BASENAME + ".csv";
 var JSON_FILENAME = __dirname + "/../" + BASENAME + ".json";
 
 module.exports = JSON.parse(fs.readFileSync(JSON_FILENAME, "utf-8"));
+
+function fixup(value, fixups) {
+  if (value in fixups) return fixups[value];
+  return value;
+}
 
 function main() {
   var data = fs.readFileSync(CSV_FILENAME, "utf-8");
@@ -18,8 +27,8 @@ function main() {
 
     data = data.map(function(columns) {
       return {
-        name: columns[0],
-        address: columns[1]
+        name: columns[1],
+        address: fixup(columns[18], ADDRESS_FIXUPS)
       };
     });
     fs.writeFileSync(JSON_FILENAME, stableStringify(data, {space: 2}));
